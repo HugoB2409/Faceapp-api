e0b510bcd7bdb7d912dae5ccc323daa3553646e0
HugoB2409 <belanger.hugo@hotmail.com>
2021-12-28 12:03:35 -0500

    Update DB connection

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,17 +1,17 @@
-const handleProgileGet = (req, res, db)=> {
-    const { id } = req.params;
-    db.select('*').from('users').where({id})
-        .then(user => {
-
-            if(user.length) {
-                res.json(user[0])
-            } else {
-                res.status(400).json('not found')
-            }
-        })
-     
-}
+const handleProgileGet = (req, res, db) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("not found");
+      }
+    });
+};
 
 module.exports = {
-    handleProgileGet
-}
+  handleProgileGet,
+};
